35ccbae6cc30d1ad80eee531ddd15604007667d0
Lex Day <lesha.dyatel@mail.ru>
2023-01-20 15:00:44 +0300

    change adaptive

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,21 @@
-  let counter = 0;
+let counter = 0;
 
-  setInterval(function(){
+setInterval(function(){
          
-         document.getElementById( 'radio' + counter.toString() ).checked = true;
+         document.getElementById('radio' + counter.toString()).checked = true;
          counter++;
          if (counter > 4){
          counter = 0;
     }
-  }, 2000);
+}, 2000);
 
 
- const pageTitle = document.title;
+const pageTitle = document.title;
 
-  window.addEventListener("blur", () => {
+window.addEventListener("blur", () => {
       document.title = "Come back here...";
-  });
+});
 
-  window.addEventListener("focus", () => {
+window.addEventListener("focus", () => {
       document.title = pageTitle;
-  });
+});
