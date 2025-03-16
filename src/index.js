db6e22f8b4fed1eb9749e5dc135c21dfc94e1cfa
Selectel Git User <git-user@selectel.ru>
2025-03-16 17:57:09 +0300

    edit skils, languages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'; // React 18
+import './index.css'; // Подключение стилей для всего приложения
+import App from './App'; // Основной компонент приложения
+import reportWebVitals from './reportWebVitals'; // Функция для измерения производительности
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
